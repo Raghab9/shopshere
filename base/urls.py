250d027444aa4ef1a968cb41bebe31bd076985b7
Raghab Prusty <raghabprusty2004@gmail.com>
2026-01-14 11:11:18 +0530

    first commit

---
+++ base/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns=[
+    path('',home,name='home'),
+    path('addtocart/<int:pk>',addtocart,name='addtocart'),
+    path('cart/',cart,name='cart'),
+    path('remove/<int:pk>',remove,name='remove'),
+    path('decrease/<int:pk>',decrease,name='decrease'),
+    path('increase/<int:pk>',increase,name='increase'),
+    path('support/',support, name='support'),
+    path('know-us/',know_us, name='know_us')
+]
